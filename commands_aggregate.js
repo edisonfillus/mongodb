0fedb6cc41c2dce8fda19547455073a2a007aef8
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-06-27 19:11:37 -0300

    Include examples of $sum, $avg and $addToSet

--- commands_aggregate.js
+++ commands_aggregate.js
@@ -1,22 +1,18 @@
-// Group students by score average
-db.grades.aggregate(
-    {
-        '$group': {
-            '_id': '$student_id',
-            'average': {$avg: '$score'}
-        }
-    },
-    {
-        '$sort': {
-            'average': -1
-        }
-    },
-    {
-        '$limit': 1
-    }
-);
+/**
+ * Aggregation Pipeline:
+ *   $project  Reshape the document
+ *   $match    Filter
+ *   $group    Aggregate
+ *   $sort     Sort
+ *   $skip     Skip
+ *   $limit    Limit
+ *   $unwind   Deconstruct Arrays
+ *   $out      Output
+ *   $geonear  Geolocalization
+ */
 
-// Group product by category
+
+// Count the number of products by category
 db.products.aggregate([
     {
         $group:
@@ -26,3 +22,109 @@ db.products.aggregate([
             }
     }
 ]);
+
+// Count the number of products by manufacturer and category
+db.products.aggregate([
+    {
+        $group:
+            {
+                _id: {
+                    "manufacturer": "$manufacturer",
+                    "category": "$category"
+                },
+                num_products: {$sum: 1}
+            }
+    }
+]);
+
+// Sum of prices by Manufacturer
+db.products.aggregate([
+    {
+        $group:
+            {
+                _id: {
+                    "manufacturer": "$manufacturer"
+                },
+                sum_prices: {$sum: "$price"}
+            }
+    }
+]);
+
+// Sum of population by State
+db.zips.aggregate([
+    {
+        $group:
+            {
+                _id: "$state",
+                "population": {$sum: "$pop"}
+            }
+    }
+]);
+
+// Average of prices by Manufacturer
+db.products.aggregate([
+    {
+        $group:
+            {
+                _id: {
+                    "manufacturer": "$manufacturer"
+                },
+                avg_price: {$avg: "$price"}
+            }
+    }
+]);
+
+// Average of State's city population
+db.zips.aggregate([
+    {
+        $group:
+            {
+                _id: "$state",
+                "avg_city_population": {$avg: "$pop"}
+            }
+    }
+]);
+
+
+
+// List of product categories by Manufacturer
+db.products.aggregate([
+    {
+        $group:
+            {
+                _id: {
+                    "manufacturer": "$manufacturer"
+                },
+                categories: {$addToSet: "$category"}
+            }
+    }
+]);
+
+// List of ZIP codes by City
+db.zips.aggregate([
+    {
+        $group:
+            {
+                _id: "$city",
+                "postal_codes": {$addToSet: "$_id"}
+            }
+    }
+]);
+
+// Group students by score average
+db.grades.aggregate(
+    {
+        $group: {
+            '_id': '$student_id',
+            'average': {$avg: '$score'}
+        }
+    },
+    {
+        $sort: {
+            'average': -1
+        }
+    },
+    {
+        $limit: 1
+    }
+);
