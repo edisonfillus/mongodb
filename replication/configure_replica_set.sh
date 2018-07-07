b9a9641cca5f791390b340283d15ed3259bf2e56
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-07-06 22:05:39 -0300

    Include examples for starting a replicaset on linux

--- replication/configure_replica_set.sh
+++ replication/configure_replica_set.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Set the primary node in replicaset and initiate
 echo 'cfg = { _id: "mySet", members:[ { _id : 0, host : "localhost:27017"} ] }; rs.initiate(cfg); ' | mongo
 wait
+
+# Set the secondary nodes
 echo 'rs.add("localhost:27018")' | mongo
 wait
 echo 'rs.add("localhost:27019")' | mongo
--- replication/create_replica_set.sh
+++ replication/create_replica_set.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+# Create data directories
+mkdir -p /data/rs1 /data/rs2 /data/rs3
 
+# Start 1st node
 mongod --replSet mySet --logpath "1.log" --dbpath /data/rs1 --port 27017 --smallfiles --oplogSize 64 --fork
- 
+
+# Start 2nd node
 mongod --replSet mySet --logpath "2.log" --dbpath /data/rs2 --port 27018 --smallfiles --oplogSize 64 --fork
 
+# Start 3rd node
 mongod --replSet mySet --logpath "3.log" --dbpath /data/rs3 --port 27019 --smallfiles --oplogSize 64 --fork
 
 
