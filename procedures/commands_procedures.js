6f3ecb4d7e6cd325c4624c0d3f06cd76896a1e00
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-07-08 10:09:02 -0300

    Include example of procedures with mongodb

---
+++ procedures/commands_procedures.js
@@ -0,0 +1,26 @@
+// Open Cursors
+var c = db.movies.find();
+c.hasNext();
+c.next();     // Bring the next item
+c.next();     // Bring the next item
+
+// Command line editing
+var j = db.names.findOne();
+j.name = "new name";
+db.names.save(j);
+
+// Delete fields
+var j = db.names.findOne();
+delete j.id;
+db.names.insertOne(j);
+
+// Replace
+var detailDoc = db.movies.findOne({"imdb.id":"tt4368814"});
+detailDoc.poster = "http://imdb.com/title/tt4368814/mediaviewer/rm2926634240";
+detailDoc.genres.push("Documentary");
+db.movies.replaceOne(
+{
+	"imdb.id": detailDoc.imdb.id
+},
+	detailDoc
+);
